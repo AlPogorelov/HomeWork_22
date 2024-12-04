00f769207e256832368252c46b2a2a5c4a648b85
Al Pogorelov <eczemplrez@gmail.com>
2024-12-04 21:07:29 +0600

    коммит на задании 3

--- HomeWork_22/catalog/models.py
+++ HomeWork_22/catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=150, verbose_name='Наименование'),
+    description = models.CharField(max_length=150, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/')
+    category = models.CharField(max_length=150, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['name']
