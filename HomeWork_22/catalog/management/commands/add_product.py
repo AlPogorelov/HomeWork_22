c35c13a5e1a9e0a1f5f1325a2c4e6babf666bf29
Al Pogorelov <eczemplrez@gmail.com>
2024-12-05 20:06:00 +0600

    done

---
+++ HomeWork_22/catalog/management/commands/add_product.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from catalog.models import Product
+
+class Command(BaseCommand):
+    help = 'Добавление тестовых продуктов в базу данных'
+
+    def handle(self, *args, **options):
+        Product.objects.all().delete()
+        call_command('loaddata', 'product_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
--- HomeWork_22/catalog/models.py
+++ HomeWork_22/catalog/models.py
@@ -1,25 +1,6 @@
 from django.db import models
 
 
-class Product(models.Model):
-
-    name = models.CharField(max_length=150, verbose_name='Наименование'),
-    description = models.CharField(max_length=150, verbose_name='Описание')
-    image = models.ImageField(upload_to='images/')
-    category = models.CharField(max_length=150, verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена за покупку')
-    created_at = models.DateField(verbose_name='Дата создания')
-    updated_at = models.DateField(verbose_name='Дата изменения')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-
-
-
 class Category(models.Model):
 
     name = models.CharField(max_length=150, verbose_name='Наименование')
@@ -32,3 +13,22 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ['name']
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='Категория')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания')
+    updated_at = models.DateField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
