93037b31cc9205bb0b697e1a9deac89d4dda46c8
Al Pogorelov <eczemplrez@gmail.com>
2024-12-11 22:35:33 +0600

    done

--- HomeWork_22/catalog/urls.py
+++ HomeWork_22/catalog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact
+from catalog.views import home, contact, product_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('contact/', contact, name='contact')
+    # path('', home, name='home'),
+    path('contact/', contact, name='contact'),
+    path('', product_list, name='product_list'),
+    path('<int:pk>', product_detail, name='product_detail')
 ]
--- HomeWork_22/catalog/views.py
+++ HomeWork_22/catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
 
 
 def home(request):
@@ -7,3 +9,16 @@ def home(request):
 
 def contact(request):
     return render(request, 'catalog/contact.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'catalog/product_detail.html', context)
+
