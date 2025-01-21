542dede4a61c291d0c6c5d2f4271efdae427da8a
Al Pogorelov <eczemplrez@gmail.com>
2025-01-21 22:15:14 +0600

    DONE

--- HomeWork_22/catalog/views.py
+++ HomeWork_22/catalog/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, ProductModeratorForm
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.utils import timezone
-from django.views.generic import DetailView, ListView, TemplateView, CreateView, DeleteView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import DetailView, ListView, TemplateView, CreateView, DeleteView, UpdateView, View
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import HttpResponseForbidden
+from django.core.exceptions import PermissionDenied
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
+    permission_required = 'catalog.add_product'
+
+    def form_valid(self, form):
+        form.instance.created_at = timezone.now().date()
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
 
-class ContactView(TemplateView):
+class ContactView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
     template_name = 'catalog/contact.html'
 
 
@@ -29,21 +38,37 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
     template_name = 'catalog/product_detail.html'
     context_object_name = 'product'
     pk_url_kwarg = 'pk'
+    # permission_required = 'catalog.view_product'
 
 
-class ProductUpdateView(LoginRequiredMixin, UpdateView):
+class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Product
     form_class = ProductForm
     template_name = 'catalog/product_form.html'
     success_url = reverse_lazy('catalog:product_list')
+    permission_required = 'catalog.change_product'
 
     def form_valid(self, form):
-        form.instance.created_at = timezone.now().date()
+        form.instance.updated_at = timezone.now().date()
         return super().form_valid(form)
 
+    def get_form_class(self):
+        user = self.request.user
+        if user == self.object.owner:
+            return ProductForm
+        if user.has_perm('catalog.can_unpublish_product'):
+            return ProductModeratorForm
+        raise PermissionDenied
 
-class ProductDeleteView(LoginRequiredMixin, DeleteView):
+
+class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_delete.html'
     pk_url_kwarg = 'pk'
     success_url = reverse_lazy('product_list')
+    permission_required = 'catalog.delete_product'
+
+    def get_form_class(self):
+        user = self.request.user
+        if not user.has_perm('catalog.delete_product'):
+            raise PermissionDenied
